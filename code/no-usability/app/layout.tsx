a14f06c28ad2b596eaeee3afffd428aec6ce4262
Emanuele Morais <emanuele.lmorais@gmail.com>
2025-11-13 22:37:15 -0300

    remove traducoes

--- code/no-usability/app/layout.tsx
+++ code/no-usability/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 
 
   return (
-    <html lang="pt-BR">
+    <html translate="no">
       <body className={`font-sans antialiased`}>
         <ClarityInit />
         <RainbowKitLayout>
